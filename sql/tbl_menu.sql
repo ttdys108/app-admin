bfd48ebe0d016ff25429c5b77b17f92cb7bfd1e9
ttdys108 <ttdys108@163.com>
2020-01-09 13:47:31 +0800

    db

--- sql/tbl_menu.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `tbl_menu` VALUES (1, 'index', '首页', NULL, '/index', 'ON', 'el-icon-s-home', 0, '2019-12-18 14:46:03', '2019-12-22 12:52:33', NULL);
-INSERT INTO `tbl_menu` VALUES (2, 'system', '系统设置', NULL, '/system', 'ON', 'el-icon-setting', 100, '2019-12-20 13:45:41', '2019-12-20 19:42:37', '');
-INSERT INTO `tbl_menu` VALUES (3, 'system_menu', '菜单设置', 2, '/system/menu', 'ON', NULL, 0, '2019-12-20 13:46:37', '2019-12-20 19:43:02', 'system_menu');
-INSERT INTO `tbl_menu` VALUES (4, 'system_dict', '字典设置', 2, '/system/dict', 'ON', NULL, 1, '2019-12-20 13:47:09', '2019-12-20 19:43:06', 'system_dict');
